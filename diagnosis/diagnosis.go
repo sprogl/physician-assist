9d22bc8688c790db3e060dd6ce08a5e78ffbf41c
sprogl <none>
2022-05-13 21:22:51 +0200

    test file for diagnosis packes created.

--- diagnosis/diagnosis.go
+++ diagnosis/diagnosis.go
@@ -1,6 +1,7 @@
 package diagnosis
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -8,24 +9,6 @@ import (
 
 	"github.com/jackc/pgx/v4"
 )
-
-var Cancer = Disease{
-	Name: "Cancer",
-	Symptoms: []string{
-		"symp1",
-		"symp2",
-		"symp3",
-	},
-}
-
-var Aids = Disease{
-	Name: "Aids",
-	Symptoms: []string{
-		"symp3",
-		"symp4",
-		"symp5",
-	},
-}
 
 //Introduce the struct Disease and some method to export its content
 type Disease struct {
@@ -40,8 +23,63 @@ type Patient struct {
 	Symptoms []string `json:"symps"`
 }
 
-func (p *Patient) IsFemale() bool {
-	return (*p).Gender == "Female"
+func (pat *Patient) IsFemale() bool {
+	return (*pat).Gender == "Female"
+}
+
+func (pat *Patient) SympsCode(dbconn *pgx.Conn) ([]byte, error) {
+	codeLength, err := SympsCodeLength(dbconn)
+	if err != nil {
+		fmt.Println("Err: line 139 of diagnosis.go")
+		return nil, err
+	}
+	code := make([]byte, codeLength)
+	rows, err := dbconn.Query(context.Background(), "SELECT id FROM symptoms_table WHERE symp IN $1;", []interface{}{(*pat).Symptoms})
+	if err != nil {
+		fmt.Println("Err: line 145 of diagnosis.go")
+		return nil, err
+	}
+	defer rows.Close()
+	var id int
+	for rows.Next() {
+		err = rows.Scan(&id)
+		if err != nil {
+			fmt.Println("Err: line 153 of diagnosis.go")
+			return nil, err
+		}
+		codeIndex := id / 8
+		code[codeIndex] |= (1 << (id % 8))
+	}
+	return code, nil
+}
+
+//This function searchs the patient's data (symtoms, gender and age) in the database and
+//finds the matched disease. It returns the result as list of matched diseases and
+//any error occurred in the process.
+func (pat *Patient) Diagnose(dbconn *pgx.Conn) ([]Disease, error) {
+	//Query to the diseases database
+	rows, err := dbconn.Query(context.Background(),
+		"SELECT name, gen, age_min, age_max, code FROM disease_table	WHERE (gen IS NULL OR gen = $1) AND (age_min <= $2 AND age_max > $2) AND ();",
+		[]interface{}{3}...) //ToDO
+	if err != nil {
+		fmt.Println("Err: line 94 of diagnosis.go")
+		return nil, err
+	}
+	defer rows.Close()
+	//Define the uninitialized disease (d) and disease slice (ds)
+	var ds []Disease
+	var d Disease
+	//Loop over the rows ofdatabase response and populate the disease slice with the return data
+	for rows.Next() {
+		err := rows.Scan(&d.Name...) //TODO
+		if err != nil {
+			fmt.Println("Err: line 105 of diagnosis.go")
+			return nil, err
+		}
+		ds = append(ds, d)
+	}
+	//Return the result
+	return []Disease{Aids, Cancer}, nil //TODO
 }
 
 //This function processes the post request
@@ -51,46 +89,54 @@ func FormProcess(req *http.Request) (*Patient, error) {
 	var p Patient
 	//Parse the posted form and extract it for further process
 	jsonDecoder := json.NewDecoder(req.Body)
-	// form := struct {
-	// 	Gender   string `json:"gen"`
-	// 	Age      int    `json:"age"`
-	// 	Symptoms string `json:"symps"`
-	// }{}
 	err := jsonDecoder.Decode(&p)
 	if err != nil {
-		fmt.Println("Err: line 40 of diagnosis.go")
+		fmt.Println("Err: line 57 of diagnosis.go")
 		return nil, err
 	}
-	//Check the gender input and set in inside the patient struct
+	//Check the gender input and set it inside the patient struct
 	switch p.Gender {
 	case "female":
 		p.Gender = "Female"
 	case "male":
 		p.Gender = "Male"
 	default:
-		fmt.Println("Err: line 52 of diagnosis.go")
-		return nil, errors.New("Wrong gender input format!")
+		fmt.Println("Err: line 67 of diagnosis.go")
+		return nil, errors.New("wrong gender input format")
 	}
 	//Check the age input and set in inside the patient struct
 	if p.Age < 0 || p.Age > 150 {
-		fmt.Println("Err: line 57 of diagnosis.go")
-		return nil, errors.New("Wrong age input format!")
+		fmt.Println("Err: line 72 of diagnosis.go")
+		return nil, errors.New("wrong age input format")
 	}
 	//Check the symptoms input and set in inside the patient struct
 	//This is done through splitting the entry by commas
 	// seperator := regexp.MustCompile(" *(([,;](\r\n|\n)* *)|([,;]*(\r\n|\n) *))")
 	// p.Symptoms = seperator.Split(form.Symptoms, -1)
 	if len(p.Symptoms) == 0 {
-		fmt.Println("Err: line 65 of diagnosis.go")
-		return nil, errors.New("Empty list of symptoms!")
+		fmt.Println("Err: line 80 of diagnosis.go")
+		return nil, errors.New("empty list of symptoms")
 	}
 	//Return the resulting patient struct and nil as the error
 	return &p, nil
 }
 
-//This function searchs the patient's data (symtoms, gender and age) in the database and
-//finds the matched disease. It returns the result as list of matched diseases and
-//any error occurred in the process.
-func (*Patient) Diagnose(dbconn *pgx.Conn) ([]Disease, error) {
-	return []Disease{Aids, Cancer}, nil
+func SympsCodeLength(dbconn *pgx.Conn) (int, error) {
+	var MaxID int
+	rows, err := dbconn.Query(context.Background(), "SELECT MAX(id) FROM symptoms_table;", []interface{}{})
+	if err != nil {
+		fmt.Println("Err: line 120 of diagnosis.go")
+		return 0, err
+	}
+	defer rows.Close()
+	err = rows.Scan(&MaxID)
+	if err != nil {
+		fmt.Println("Err: line 126 of diagnosis.go")
+		return 0, err
+	}
+	codeLength := (MaxID + 1) / 8
+	if MaxID-(codeLength*8)+1 > 0 {
+		codeLength += 1
+	}
+	return codeLength, nil
 }
--- main.go
+++ main.go
@@ -17,6 +17,14 @@ import (
 //Here, we define the templates as global viriables to be reachable within all functions
 var dbconn *pgx.Conn
 
+//Following environment variables are needed for this programm to run
+//1- DIAGAPIPORT	the port on which the programm responds
+//2- DBUSER			the databse username
+//3- DBPASS			the database password
+//4- DBIP			the databse IP
+//5- DBPORT			the databse port
+//6- DATABASE		the database name
+
 //Some structs to deal with data used in program
 type resultPage struct {
 	Items []diagnosis.Disease `json:"diseases"`
@@ -28,20 +36,15 @@ func dignosisFormHandler(wr http.ResponseWriter, req *http.Request) {
 	//This passes the post request to the formProcess function and gets the patient struct
 	pat, err := diagnosis.FormProcess(req)
 	if err != nil {
-		fmt.Println("Err: line 53 of main.go")
+		fmt.Println("Err: line 39 of main.go")
 		http.Error(wr, err.Error(), http.StatusBadRequest)
 		return
-	}
-	//Print out the passed symptoms
-	symps := pat.Symptoms
-	for i := 0; i < len(symps); i++ {
-		fmt.Println(symps[i])
 	}
 	//Analyse the symtoms and get the list of matched diseases
 	diseases, err := pat.Diagnose(dbconn)
 	if err != nil {
-		fmt.Println("Err: line 65 of main.go")
-		http.Error(wr, err.Error(), http.StatusBadRequest)
+		fmt.Println("Err: line 46 of main.go")
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	//Prepare the data to feed into the template
@@ -51,12 +54,12 @@ func dignosisFormHandler(wr http.ResponseWriter, req *http.Request) {
 		Items: diseases,
 	})
 	if err != nil {
-		fmt.Println("Err: line 76 of main.go")
-		http.Error(wr, err.Error(), http.StatusBadRequest)
+		fmt.Println("Err: line 57 of main.go")
+		http.Error(wr, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	//Declare that the response data will be in json format
-	wr.Header().Add("Access-Control-Allow-Origin", "*")
+	// wr.Header().Add("Access-Control-Allow-Origin", "*")
 	wr.Header().Set("Content-Type", "application/json")
 	//Feed the data into the result page template and serve it
 	fmt.Fprintf(wr, string(dataJson))
@@ -68,14 +71,18 @@ func main() {
 	//Initiate the database connection
 	dbconn, err := pgx.Connect(context.Background(), DBAdrress)
 	if err != nil {
+		fmt.Println("Err: line 74 of main.go")
 		log.Fatal(err)
 	}
 	defer dbconn.Close(context.Background())
-
 	//Initialize the mux router
 	router := mux.NewRouter().StrictSlash(true)
 	//Set the respective handlers to uri addresses
 	router.HandleFunc("/diagnosis/v1/index.html", dignosisFormHandler).Methods("Post")
 	//Listen to the defined port and serve
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("DIAGAPIPORT")), router))
+	err = http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("DIAGAPIPORT")), router)
+	if err != nil {
+		fmt.Println("err: line 85 of main.go")
+		log.Fatal(err)
+	}
 }
